6333d7585da1f72246e9d8343fa53921451738ae
unknown <donald@peaksolutionsid.com>
2024-10-14 15:30:49 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,65 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+'use client'
+import { useState, ChangeEvent, FormEvent } from "react"
+import { Center, Stack, FormControl, Input, InputGroup, InputRightElement, Button, AspectRatio } from "@chakra-ui/react"
+import { Icon } from "@chakra-ui/react"
+import { FaPaste } from "react-icons/fa"
+
+export default function Page() {
+  const [url, setUrl] = useState("https://www.instagram.com/p/DBAuEWcNllf/")
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const accessToken = 'IGQWRQUkhDbDBuWVJDcWZAEdTdyaGlvcklHSTZAfSTkwM2xuYXZA5Q20wdXdUSzQ2eFUxQlVMQ2hYRnUxbml3MnBnM0hUbW1aSG9SbXdnMTAwRHhrQ0sxSXVYV0FfVl9FU1JQazN4a2tRTGwzQjNMR095TWtQYUYtd0kZD'
+
+  const getInstagramShortcode = (urlShort: string) => {
+    const regex = /(?:https?:\/\/)?(?:www\.)?instagram\.com\/p\/([A-Za-z0-9_-]+)/;
+    const match = urlShort.match(regex);
+    return match ? match[1] : null;
+  }
+
+  const submit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+
+    const shortcode = getInstagramShortcode(url);
+    const apiUrl = `https://graph.instagram.com/${shortcode}?fields=caption&access_token=${accessToken}`;
+
+    if (!shortcode) {
+      console.error('Invalid Instagram URL');
+      return;
+    } else {
+      console.log(shortcode)
+    }
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      console.log(data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Center height='100vh'>
+      <Stack>
+        <form onSubmit={(e: FormEvent<HTMLFormElement>) => submit(e)}>
+          <FormControl>
+            <InputGroup>
+              <Input type="text" minW={550} value={url} onChange={(e: ChangeEvent<HTMLInputElement>) => setUrl(e.target.value)} placeholder='Paste URL Instagram' />
+              <InputRightElement>
+                <Button >
+                  <Icon as={FaPaste} color='#493628' />
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <Button type="submit" colorScheme='red' size='sm' mt={2}>Download</Button>
+          </FormControl>
+        </form>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        {/* <AspectRatio ratio={4 / 3} mt={10}>
+          <iframe src={url} />
+        </AspectRatio> */}
+      </Stack>
+    </Center>
+  )
 }
