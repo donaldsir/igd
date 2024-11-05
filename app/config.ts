3cf1fd35600fe545d2e5a69670d1d95362d759c2
unknown <donald@peaksolutionsid.com>
2024-11-05 11:25:14 +0800

    update frame maker

--- app/config.ts
+++ app/config.ts
@@ -27,3 +27,18 @@ export const dimensiAlfabet = [
     { huruf: "Z", panjang: 15.32 }, { huruf: "z", panjang: 12.53 },
     { huruf: " ", panjang: 6.96 } // Spasi
 ];
+
+export const words: string[] = [
+    "the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog",
+    "lorem", "ipsum", "dolor", "sit", "amet", "consectetur",
+    "adipiscing", "elit", "sed", "do", "eiusmod", "tempor",
+    "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua",
+    "ut", "enim", "ad", "minim", "veniam", "quis", "nostrud",
+    "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip",
+    "ex", "ea", "commodo", "consequat", "duis", "aute", "irure",
+    "dolor", "in", "reprehenderit", "in", "voluptate", "velit",
+    "esse", "cillum", "dolore", "eu", "fugiat", "nulla", "pariatur",
+    "excepteur", "sint", "occaecat", "cupidatat", "non", "proident",
+    "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit",
+    "anim", "id", "est", "laborum"
+];
--- app/frame/page.tsx
+++ app/frame/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, FormEvent, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import {
   FormControl,
   Input,
@@ -18,13 +18,13 @@ import {
   FormLabel,
   Text,
   Center,
+  Container
 } from "@chakra-ui/react";
 import { useToast } from "@chakra-ui/react";
 import { FaArrowLeft } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import * as htmlToImage from "html-to-image";
 import { Roboto } from 'next/font/google'
-import { dimensiAlfabet } from "../config";
 
 const roboto = Roboto({
   weight: '700',
@@ -36,8 +36,7 @@ export default function Page() {
   const toast = useToast();
 
   const [gambar, setGambar] = useState("");
-  const [title, setTitle] = useState(`Meutya Buka Peluang Polisi`);
-  const [lines, setLines] = useState<string[]>([]);
+  const [title, setTitle] = useState(``);
 
   const showToast = useCallback(
     async (title: string, iStatus: number, message: string) => {
@@ -69,61 +68,33 @@ export default function Page() {
     setGambar(imgsrc);
   };
 
-  const randomSentence = useCallback((maxLength: number = 100): string => {
-    // Define a list of words to choose from
-    const words: string[] = [
-      "the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog",
-      "lorem", "ipsum", "dolor", "sit", "amet", "consectetur",
-      "adipiscing", "elit", "sed", "do", "eiusmod", "tempor",
-      "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua",
-      "ut", "enim", "ad", "minim", "veniam", "quis", "nostrud",
-      "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip",
-      "ex", "ea", "commodo", "consequat", "duis", "aute", "irure",
-      "dolor", "in", "reprehenderit", "in", "voluptate", "velit",
-      "esse", "cillum", "dolore", "eu", "fugiat", "nulla", "pariatur",
-      "excepteur", "sint", "occaecat", "cupidatat", "non", "proident",
-      "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit",
-      "anim", "id", "est", "laborum"
-    ];
+  // const randomSentence = useCallback((maxLength: number = 100): string => {
+  //   let sentence = '';
 
-    let sentence = '';
+  //   // Continue adding words until the sentence reaches the maximum length
+  //   while (true) {
+  //     // Randomly select a word from the list
+  //     const word = words[Math.floor(Math.random() * words.length)];
 
-    // Continue adding words until the sentence reaches the maximum length
-    while (true) {
-      // Randomly select a word from the list
-      const word = words[Math.floor(Math.random() * words.length)];
+  //     // Check if adding the next word exceeds the max length
+  //     if (sentence.length + word.length + 1 > maxLength) {
+  //       break; // Stop if the next word would exceed max length
+  //     }
 
-      // Check if adding the next word exceeds the max length
-      if (sentence.length + word.length + 1 > maxLength) {
-        break; // Stop if the next word would exceed max length
-      }
+  //     // Add the word to the sentence
+  //     sentence += (sentence ? ' ' : '') + word; // Add space if not the first word
+  //   }
 
-      // Add the word to the sentence
-      sentence += (sentence ? ' ' : '') + word; // Add space if not the first word
-    }
+  //   // Capitalize the first letter and add a period at the end
+  //   sentence = sentence.charAt(0) + sentence.slice(1);
 
-    // Capitalize the first letter and add a period at the end
-    sentence = sentence.charAt(0) + sentence.slice(1) + '.';
+  //   return sentence;
+  // }, [toast, showToast]);
 
-    return sentence;
-  }, [toast, showToast]);
+  // useEffect(() => {
+  //   setTitle(randomSentence());
+  // }, [randomSentence]);
 
-  useEffect(() => {
-    setTitle(randomSentence());
-  }, [randomSentence]);
-
-  const submit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const trimTitle = capitalizeFirstLetter(title.trim());
-    if (trimTitle.length > 110) {
-      showToast("Error", 1, `Title is too long (maximum is 110 characters)`);
-      return;
-    }
-
-    console.log(textWidth("consequat esse dolor commodo"))
-    setLines(splitText(trimTitle))
-  };
 
   const createFileName = () => {
     // Generate a random string
@@ -154,64 +125,6 @@ export default function Page() {
     });
   };
 
-  const capitalizeFirstLetter = (text: string) => {
-    if (!text) return "";
-    return text.charAt(0).toUpperCase() + text.slice(1);
-  }
-
-  const textWidth = (text: string) => {
-    let totalWidth = 0;
-
-    for (const char of text) {
-      const dimensi = dimensiAlfabet.find(d => d.huruf === char);
-      if (dimensi) {
-        totalWidth += dimensi.panjang;
-      }
-    }
-
-    return totalWidth;
-  }
-
-  const splitText = (text: string) => {
-    const hasil: string[] = [];
-    const kataArray = text.split(" "); // Memecah kalimat menjadi array kata
-
-    let kalimatBagian = "";
-    let totalPanjang = 0;
-    const spaceWidth = textWidth(" ")
-
-    for (const kata of kataArray) {
-      let kataPanjang = 0;
-
-      // Hitung panjang kata
-      for (const char of kata) {
-        const dimensi = dimensiAlfabet.find(d => d.huruf === char);
-        if (dimensi) {
-          kataPanjang += dimensi.panjang;
-        }
-      }
-
-      // Cek apakah menambahkan kata ini melebihi batas panjang
-      if (totalPanjang + kataPanjang > 330) {
-        // Jika ya, tambahkan bagian kalimat ke hasil dan mulai bagian baru
-        hasil.push(kalimatBagian.trim());
-        kalimatBagian = kata; // Mulai kalimat baru dengan kata ini
-        totalPanjang = kataPanjang; // Reset total panjang
-      } else {
-        // Jika tidak, tambahkan kata ke bagian kalimat
-        kalimatBagian += (kalimatBagian ? " " : "") + kata; // Menambahkan spasi jika sudah ada kata
-        totalPanjang += kataPanjang;
-      }
-    }
-
-    // Tambahkan bagian terakhir jika ada
-    if (kalimatBagian) {
-      hasil.push(kalimatBagian.trim());
-    }
-
-    return hasil.reverse();
-  }
-
 
   return (
     <VStack divider={<StackDivider borderColor="gray.200" />} align="stretch">
@@ -233,41 +146,34 @@ export default function Page() {
         <SimpleGrid columns={{ md: 2, sm: 1 }} m={4} spacing={8}>
           <Card>
             <CardBody>
-              <form onSubmit={(e: FormEvent<HTMLFormElement>) => submit(e)}>
-                <FormControl>
-                  <FormLabel>Image</FormLabel>
-                  <Input type="file" accept="image/*" size="sm" onChange={(e) => onChangeFile(e)} />
-                </FormControl>
-                <FormControl mt={4}>
-                  <FormLabel>
-                    Title <span style={{ color: "red", fontSize: 14 }}>({`${title.trim().length}/110`})</span>
-                  </FormLabel>
-                  <Textarea
-                    value={title}
-                    style={{ whiteSpace: "pre-wrap" }}
-                    size="sm"
-                    rows={3}
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
-                </FormControl>
-                <Button type="submit" colorScheme="teal" size="sm" mt={4}>
-                  Create Text
-                </Button>
-                <Button
-                  onClick={() => download("canvas", createFileName())}
-                  colorScheme="teal"
+              <FormControl>
+                <FormLabel>Image</FormLabel>
+                <Input type="file" accept="image/*" size="sm" onChange={(e) => onChangeFile(e)} />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>
+                  Title <span style={{ color: "red", fontSize: 14 }}>({`${title.trim().length}/100`})</span>
+                </FormLabel>
+                <Textarea
+                  value={title}
+                  style={{ whiteSpace: "pre-wrap" }}
                   size="sm"
-                  mt={4}
-                  ml={1}
-                  disabled={gambar ? false : true}
-                >
-                  Download Result
-                </Button>
-              </form>
+                  rows={3}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </FormControl>
+
+              <Button
+                onClick={() => download("canvas", createFileName())}
+                colorScheme="teal"
+                size="sm"
+                mt={4}
+                ml={1}
+              >
+                Download Result
+              </Button>
             </CardBody>
           </Card>
-
-
           <Center id="canvas" style={{ position: "relative", width: 400, height: 500 }}>
             <Image
               src="/images/logo-pd.png"
@@ -276,23 +182,21 @@ export default function Page() {
               alt="logo white"
             />
             <Image src={gambar ? gambar : "/images/no-image.jpg"} w={400} h={500} fit="cover" alt="media" />
-            <Box
-              style={{ position: "absolute", bottom: 50, boxShadow: '5px 5px #148b9d' }}
-              bg="rgba(255,255,255,0.7)"
+            <Container
+              style={{ position: "absolute", bottom: 60, boxShadow: '7px 7px #148b9d' }}
+              bg="rgba(255,255,255,0.9)"
               w="85%"
-              h={45 * lines.length}
-            />
-            {lines.map((item, index) => (
+              p={2}
+            >
               <Text
-                key={index}
-                style={{ position: "absolute", top: 400 - 37 * index }}
                 fontSize={24}
                 px={1}
                 className={roboto.className}
+                textAlign="center"
               >
-                {item}
+                {title}
               </Text>
-            ))}
+            </Container>
           </Center>
         </SimpleGrid>
       </Box>
