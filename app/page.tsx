b5a56713cb2ed68de6e593b482b2d4237c089bf6
donaldpsg <donald.peaksolutions@gmail.com>
2025-03-19 17:27:13 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Center, VStack, Button, Image, Box, SimpleGrid } from "@chakra-ui/react";
+import { Center, VStack, Button, Image, Box, SimpleGrid, Heading, Text } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
@@ -9,23 +9,34 @@ export default function Page() {
     <Center height="50vh">
       <VStack>
         <Image src="/images/logo-pd.png" alt="logo" h={128} />
+
         <Box mt={10} borderWidth={2} borderRadius={10} p={4}>
-          <SimpleGrid columns={2} spacing={2}>
+          <SimpleGrid w={400}>
+            <Heading fontSize="xl" textAlign="center">
+              All-in-One Media App
+            </Heading>
+            <Text mt={2} textAlign="justify">
+              This application offers multiple features to enhance your digital experience and simplify daily tasks.
+              Whether you need to check the Mobile Driving License Service (SIM Keliling) schedule, add text to Reels
+              and videos, or download media from a feed, this app has you covered!
+            </Text>
+          </SimpleGrid>
+          <SimpleGrid columns={2} spacing={2} mt={4}>
             <Button
               colorScheme="teal"
               onClick={() => {
                 router.push(`/download`);
               }}
             >
-              IG POST
+              Instagram Downloader
             </Button>
             <Button
               colorScheme="teal"
               onClick={() => {
-                router.push(`/twitter`);
+                router.push(`/sim`);
               }}
             >
-              X VIDEO
+              Driving License Renewal
             </Button>
           </SimpleGrid>
           <SimpleGrid columns={2} spacing={2} mt={2}>
@@ -35,33 +46,15 @@ export default function Page() {
                 router.push(`/video`);
               }}
             >
-              IG VIDEO
+              Text on Reels
             </Button>
-            <Button
-              colorScheme="teal"
-              onClick={() => {
-                router.push(`/sim`);
-              }}
-            >
-              SIM KELILING
-            </Button>
-          </SimpleGrid>
-          <SimpleGrid columns={2} spacing={2} mt={2}>
             <Button
               colorScheme="teal"
               onClick={() => {
                 router.push(`/watermark`);
               }}
             >
-              WATERMARK VIDEO
-            </Button>
-            <Button
-              colorScheme="teal"
-              onClick={() => {
-                router.push(`/cloudinary`);
-              }}
-            >
-              CLOUDINARY
+              Text on Video
             </Button>
           </SimpleGrid>
         </Box>
